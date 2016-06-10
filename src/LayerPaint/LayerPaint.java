228f3c909947a98552406ecb8209f8b3dcab299a
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2016-06-10 14:39:30 +0200

    Deleted Netbeans design guff

--- src/LayerPaint/LayerPaint.java
+++ src/LayerPaint/LayerPaint.java
@@ -2,11 +2,11 @@ package LayerPaint;
 
 import javax.swing.SwingUtilities;
 
-public class Colorclicker {
+public class LayerPaint {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new _Window_bu();
+                new Window();
             }
         });
     }
