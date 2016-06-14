2d2398dc5d589bd726152c23598b4b0ef092a083
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2016-06-14 20:25:53 +0200

    Removed LaF, not working on Linux

--- src/LayerPaint/LayerPaint.java
+++ src/LayerPaint/LayerPaint.java
@@ -8,15 +8,6 @@ public class LayerPaint {
 
     public static void main(String[] args) {
         
-        try {
-            // Set System L&F
-        UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
-        } 
-        catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-           System.out.println("Failed to load LAF");
-        }
-        
         SwingUtilities.invokeLater(() -> {
             Window window = new Window();
         });
