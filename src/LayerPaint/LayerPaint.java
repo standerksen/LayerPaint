eb132b8f92d07ea50bfb5cfced033190d944ef09
Pieter van Laatum <Pieter.laatum@student.ru.nl>
2016-06-14 19:11:54 +0200

    L&F

--- src/LayerPaint/LayerPaint.java
+++ src/LayerPaint/LayerPaint.java
@@ -1,10 +1,31 @@
 package LayerPaint;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class LayerPaint {
 
     public static void main(String[] args) {
+        
+        try {
+            // Set System L&F
+        UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+           // handle exception
+        }
+        catch (InstantiationException e) {
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+           // handle exception
+    }
+        
         SwingUtilities.invokeLater(() -> {
             Window window = new Window();
         });
